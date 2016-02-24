61e0db7b9253aba1833d84def1f4f9a22931da24
浅井亮 <leo0131asai@gmail.com>
2016-02-24 14:51:49 +0900

    adding emil

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,22 +6,20 @@ class ChargesController < ApplicationController
       :email => params[:stripeEmail],
       :source  => params[:stripeToken]
     )
-
+　　
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => params[:amount],
       :description => 'Growth Hacking Crash Course',
       :currency    => 'jpy'
     )
-
+  
     purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], amount: params[:amount], description: charge.description, currency: charge.currency, customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
 
     redirect_to purchase
-
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
+
   end
-
-
 end
